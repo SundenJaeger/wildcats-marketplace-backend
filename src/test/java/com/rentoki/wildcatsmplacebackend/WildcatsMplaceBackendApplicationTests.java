fc61ce65ef07d0cc7e54ea29ec5497ce25859a27
Rentoki <knowingisnotenoughwemustapply1@gmail.com>
2025-09-20 14:44:15 +0800

    Initial commit

---
+++ src/test/java/com/rentoki/wildcatsmplacebackend/WildcatsMplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentoki.wildcatsmplacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcatsMplaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
