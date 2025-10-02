1f09bcf1f7f132ca86bb561a595b34e8e2d2ae3b
Rentoki <knowingisnotenoughwemustapply1@gmail.com>
2025-10-02 12:52:54 +0800

    Add AdminRepository

---
+++ src/main/java/com/rentoki/wildcatsmplacebackend/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.rentoki.wildcatsmplacebackend.repository;
+
+import com.rentoki.wildcatsmplacebackend.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+    List<Admin> findByDepartment(String department);
+    List<Admin> findByRole(String role);
+}
